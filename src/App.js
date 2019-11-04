72e29f466b37c45d944503d86c1934ed1ea997cb
mew101 <user@Users-MacBook-Air-5.local>
2019-11-04 09:21:44 -0500

    search-bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// eslint-disable-next-line
+import Logo from './logo.svg';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = 'e03c014b'
   const APP_KEY = '81c37b55a389866039cfd9824b61039c	'
+// eslint-disable-next-line
+  const SampleReq =
+   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  const sampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"free
-;
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <form className="search-form">
+        <input className="search-bar" type = "text" />
+        <button className="search-button" type = "submit">Search</button>
+      </form>
     </div>
   );
 }
