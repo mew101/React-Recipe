0fc3b2e0a5b6350704ee52eb2d906dd937ababe4
mew101 <user@Users-MacBook-Air-5.local>
2019-11-03 19:23:49 -0500

    adding api

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'e03c014b'
+  const APP_KEY = '81c37b55a389866039cfd9824b61039c	'
+
+  const sampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"free
+;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello react</h1>
     </div>
   );
 }
