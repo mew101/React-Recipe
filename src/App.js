bade0a38079951b5734e3a326a6b185cd860d0bd
mew101 <user@Users-MacBook-Air-5.local>
2019-11-04 11:29:48 -0500

    working incrementer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from 'react';
-// eslint-disable-next-line
-import Logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -11,12 +9,19 @@ const App = () => {
   const SampleReq =
    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
+   const [counter, setCounter] = useState(0);
+
+   useEffect(() => {
+     console.log("Effect has been run");
+   });
+
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type = "text" />
-        <button className="search-button" type = "submit">Search</button>
+        <button className="search-button" type = "submit">search</button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
