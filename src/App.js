0325bc3218df8712582f643023a04a911993d58b
mew101 <user@Users-MacBook-Air-5.local>
2019-11-05 00:12:49 -0500

    fetching api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React,{useEffect, useState} from 'react';
+import React,{useEffect} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = 'e03c014b'
   const APP_KEY = '81c37b55a389866039cfd9824b61039c	'
-// eslint-disable-next-line
-  const SampleReq =
-   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-   const [counter, setCounter] = useState(0);
 
-   useEffect(() => {
-     console.log("Effect has been run");
-   });
+   useEffect( () => {
+     getRecipes();
+   }, []);
 
+   const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+   }
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type = "text" />
         <button className="search-button" type = "submit">search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
